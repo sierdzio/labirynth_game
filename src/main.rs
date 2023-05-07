b514a195f740f08e34c655b6b5ed526e1b09d0b7
Tomasz Siekierda <sierdzio@gmail.com>
2023-05-07 10:13:29 +0200

    Slint GUI testing - displays a nice grid!

--- src/main.rs
+++ src/main.rs
@@ -154,8 +154,15 @@ fn main() {
 }
 
 slint::slint! {
+    struct TileData {
+        character: string,
+        textColor: color
+    }
+
     component Tile inherits Rectangle {
         in property <string> character;
+        in property <color> textColor: #000000;
+        in property <image> icon;
 
         width: 24px;
         height: 24px;
@@ -163,13 +170,46 @@ slint::slint! {
     
         Text {
             text: parent.character;
-            color: #000000;
+            color: parent.textColor;
+        }
+
+        Image {
+            source: parent.icon;
         }
     }
 
     export component MainWindow inherits Window {
-        Tile {
-            character: "w";
+        width: 300px;
+        height: 300px;
+
+        in property <[TileData]> tiles: [
+            { character: "0" },
+            { character: "1" },
+            { character: "2" },
+            { character: "3" },
+            { character: "4" },
+            { character: "5" },
+            { character: "6" },
+            { character: "7" },
+            { character: "8" },
+            { character: "9" },
+            { character: "10" },
+        ];
+
+        in property <int> columnCount: 4;
+        in property <int> rowCount: 3;
+
+        VerticalLayout {
+            spacing: 1px;
+
+            for row in rowCount: HorizontalLayout {
+                for column in columnCount: Tile {
+                    preferred-height: 15px;
+                    preferred-width: 15px;
+
+                    character: tiles[(row * columnCount) + column].character;
+                }
+            }
         }
     }
 }
