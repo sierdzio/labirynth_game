bb2ae88a5fa06f5d40049468b50d42aee9a32c72
Tomasz Siekierda <sierdzio@gmail.com>
2023-05-13 10:03:32 +0200

    Add command line options to lauch CLI or launcher.

---
+++ src/gui.rs
@@ -0,0 +1,70 @@
+//use crate::player;
+//use crate::board;
+//use crate::levels;
+
+pub fn run_game_gui() {
+    MainWindow::new().unwrap().run().unwrap();
+}
+
+slint::slint! {
+    struct TileData {
+        character: string,
+        textColor: color
+    }
+
+    component Tile inherits Rectangle {
+        in property <string> character;
+        in property <color> textColor: #000000;
+        in property <image> icon;
+
+        width: 24px;
+        height: 24px;
+        background: #12aa12;
+    
+        Text {
+            text: parent.character;
+            color: parent.textColor;
+        }
+
+        Image {
+            source: parent.icon;
+        }
+    }
+
+    export component MainWindow inherits Window {
+        width: 300px;
+        height: 300px;
+
+        in property <[TileData]> tiles: [
+            { character: "0" },
+            { character: "1" },
+            { character: "2" },
+            { character: "3" },
+            { character: "4" },
+            { character: "5" },
+            { character: "6" },
+            { character: "7" },
+            { character: "8" },
+            { character: "9" },
+            { character: "10" },
+        ];
+
+        in property <int> columnCount: 4;
+        in property <int> rowCount: 3;
+
+        VerticalLayout {
+            spacing: 1px;
+
+            for row in rowCount: HorizontalLayout {
+                spacing: 1px;
+                
+                for column in columnCount: Tile {
+                    preferred-height: 15px;
+                    preferred-width: 15px;
+
+                    character: tiles[(row * columnCount) + column].character;
+                }
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -2,76 +2,26 @@ pub mod player;
 pub mod board;
 pub mod levels;
 pub mod cli;
+pub mod gui;
 
 fn main() {
-    let launch_gui = cli::make_ui_choice();
+    let mut launch_gui = true;
+    let mut launcher = false;
+    for arg in std::env::args() {
+        if arg == "--cli" {
+            launch_gui = false;
+        } else if arg == "--choose" {
+            launcher = true;
+        }
+    }
+
+    if launcher {
+        launch_gui = cli::make_ui_choice();
+    }
 
     if launch_gui {
-        MainWindow::new().unwrap().run().unwrap();
+        gui::run_game_gui();
     } else {
         cli::run_game_cli();
-    }
-}
-
-slint::slint! {
-    struct TileData {
-        character: string,
-        textColor: color
-    }
-
-    component Tile inherits Rectangle {
-        in property <string> character;
-        in property <color> textColor: #000000;
-        in property <image> icon;
-
-        width: 24px;
-        height: 24px;
-        background: #12aa12;
-    
-        Text {
-            text: parent.character;
-            color: parent.textColor;
-        }
-
-        Image {
-            source: parent.icon;
-        }
-    }
-
-    export component MainWindow inherits Window {
-        width: 300px;
-        height: 300px;
-
-        in property <[TileData]> tiles: [
-            { character: "0" },
-            { character: "1" },
-            { character: "2" },
-            { character: "3" },
-            { character: "4" },
-            { character: "5" },
-            { character: "6" },
-            { character: "7" },
-            { character: "8" },
-            { character: "9" },
-            { character: "10" },
-        ];
-
-        in property <int> columnCount: 4;
-        in property <int> rowCount: 3;
-
-        VerticalLayout {
-            spacing: 1px;
-
-            for row in rowCount: HorizontalLayout {
-                spacing: 1px;
-                
-                for column in columnCount: Tile {
-                    preferred-height: 15px;
-                    preferred-width: 15px;
-
-                    character: tiles[(row * columnCount) + column].character;
-                }
-            }
-        }
     }
 }
