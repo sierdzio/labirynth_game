f3fa353483b381bdaababb287e686fb4c4cd57aa
Tomasz Siekierda <sierdzio@gmail.com>
2023-05-13 09:29:45 +0200

    Move CLI part to cli.rs. Not fully separated game logic yet

--- src/main.rs
+++ src/main.rs
@@ -1,155 +1,15 @@
-use std::io::{Write, stdout, stdin};
-
-use termion::input::TermRead;
-use termion::event::Key;
-use termion::raw::IntoRawMode;
-
-use std::{fs, process};
-
 pub mod player;
 pub mod board;
 pub mod levels;
-
-const NEW_LINE: &str = "\r\n";
-
-fn draw_ui_choice(launch_gui: bool) {
-    let mut std_out = stdout().into_raw_mode().unwrap();
-    write!(std_out, "{}", termion::cursor::Hide).unwrap();
-    write!(std_out, "{}", termion::clear::All).unwrap();
-    write!(std_out, "{}", termion::cursor::Goto(1, 1)).unwrap();
-
-    let cli_char = match launch_gui {
-        true => '-',
-        false => '*'
-    };
-
-    let gui_char = match launch_gui {
-        true => '*',
-        false => '-'
-    };
-
-    write!(std_out, "Play in terminal or GUI? (press 'q' to quit){}", NEW_LINE).unwrap();
-    write!(std_out, "{} CLI{}", cli_char, NEW_LINE).unwrap();
-    write!(std_out, "{} GUI{}", gui_char, NEW_LINE).unwrap();
-}
-
-fn make_ui_choice() -> bool {
-    let std_in = stdin();
-    let mut std_out = stdout().into_raw_mode().unwrap();
-    write!(std_out, "{}", termion::cursor::Hide).unwrap();
-
-    let mut launch_gui = false;
-
-    draw_ui_choice(launch_gui);
-
-    for c in std_in.keys() {
-        match c.unwrap() {
-            Key::Char('q') => {
-                write!(std_out, "Quitting!{}", NEW_LINE).unwrap();
-                write!(std_out, "{}", termion::cursor::Show).unwrap();
-                process::exit(0);
-            },          
-            Key::Up => {
-                launch_gui = false;
-            },
-            Key::Down => {
-                launch_gui = true;
-            },
-            Key::Char(' ') => {
-                break;
-            },
-            _ => {
-                // Nothing
-            }
-        }
-
-        draw_ui_choice(launch_gui);
-    }
-
-    return launch_gui;
-}
+pub mod cli;
 
 fn main() {
-    let launch_gui = make_ui_choice();
+    let launch_gui = cli::make_ui_choice();
 
     if launch_gui {
         MainWindow::new().unwrap().run().unwrap();
     } else {
-        let mut std_out = stdout().into_raw_mode().unwrap();
-        write!(std_out, "{}", termion::cursor::Hide).unwrap();
-
-        let std_in = stdin();
-        let level = levels::let_user_select_level(&mut std_out);
-
-        // Read board data
-        let path = match level {
-            Ok(level) => format!("boards/{}", level),
-            Err(message) => panic!("Invalid level selected: {}", message),
-        };
-
-        let raw_data = match fs::read_to_string(path) {
-            Ok(data) => data,
-            Err(error) => panic!("{}", error),
-        };
-
-        let board: Vec<Vec<char>> = match board::parse_board_string(&mut std_out, raw_data) {
-            Ok(data) => data,
-            Err(error) => panic!("{}", error),
-        };
-
-        let mut position = player::Position {
-            x: 0,
-            y: 0
-        };    
-
-        board::draw_board(&mut std_out, &board, &position);
-
-        let mut direction: player::Direction;
-
-        for c in std_in.keys() {
-            write!(std_out, "{}{}", termion::clear::All, termion::cursor::Goto(1, 1)).unwrap();
-
-            let mut move_text: &str = "";
-
-            match c.unwrap() {
-                Key::Char('q') => {
-                    write!(std_out, "Quitting!{}", NEW_LINE).unwrap();
-                    write!(std_out, "{}", termion::cursor::Show).unwrap();
-                    process::exit(0);
-                },
-                Key::Left => {
-                    move_text = "Moving left\r\n";
-                    direction = player::Direction::Left;
-                },
-                Key::Right => {
-                    move_text = "Moving right\r\n";
-                    direction = player::Direction::Right;
-                },            
-                Key::Up => {
-                    move_text = "Moving up\r\n";
-                    direction = player::Direction::Up;
-                },
-                Key::Down => {
-                    move_text = "Moving down\r\n";
-                    direction = player::Direction::Down;
-                },
-                _ => {
-                    direction = player::Direction::Unknown;
-                }
-            }
-
-            player::try_to_move(&board, &mut position, direction);
-            board::draw_board(&mut std_out, &board, &position);
-            write!(std_out, "{} {}", move_text, NEW_LINE).unwrap();
-
-            if board[position.y][position.x] == board::FINISH_TILE
-                || board[position.y][position.x] == board::ALTERNATE_FINISH_TILE {
-                write!(std_out, "\nSuccess! Congratulations!\n").unwrap();
-                break;
-            }
-        }
-
-        write!(std_out, "{}", termion::cursor::Show).unwrap();
+        cli::run_game_cli();
     }
 }
 
@@ -203,6 +63,8 @@ slint::slint! {
             spacing: 1px;
 
             for row in rowCount: HorizontalLayout {
+                spacing: 1px;
+                
                 for column in columnCount: Tile {
                     preferred-height: 15px;
                     preferred-width: 15px;
